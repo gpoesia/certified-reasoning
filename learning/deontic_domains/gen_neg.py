d50391516f6b3df01e38e123beffdb208474c06e
Kanishk Gandhi <kanishk050296@gmail.com>
2023-05-12 17:05:24 -0700

    add more negated

--- learning/deontic_domains/gen_neg.py
+++ learning/deontic_domains/gen_neg.py
@@ -11,32 +11,30 @@ def parse_and_negate_problem(file_path):
     # Extract the question
     question = problem_text[1].strip()
 
-    # Replace terms with their negations
-    negation_dict = {
-        'permissible': 'impermissible',
-        'impermissible': 'permissible',
-        'obligatory': 'not obligatory',
-        'not obligatory': 'obligatory'
-    }
-
     if 'impermissible' in question:
-        question = question.replace('impermissible', 'permissible')
+        question_1 = question.replace('impermissible', 'permissible')
+        question_2 = question.replace('impermissible', 'obligatory')
     elif 'permissible' in question:
-        question = question.replace('permissible', 'impermissible')
+        question_1 = question.replace('permissible', 'impermissible')
+        question_2 = question.replace('permissible', 'obligatory')
     elif 'not obligatory' in question:
-        question = question.replace('not obligatory', 'obligatory')
+        question_1 = question.replace('not obligatory', 'obligatory')
+        question_2 = question.replace('not obligatory', 'impermissible')
     elif 'obligatory' in question:
-        question = question.replace('obligatory', 'not obligatory')
+        question_1 = question.replace('obligatory', 'not obligatory')
+        question_2 = question.replace('obligatory', 'impermissible')
 
     answer = "Answer (Yes or no): No"
-    return context, question, answer
+    return context, [question_1, question_2], answer
 
 files = sorted([f for f in os.listdir('./') if f.endswith('.txt') and 'negated' not in f])
 file_template = '{context}\n{question}\nReasoning:\n{answer}'
 for f in files: 
-    context, question, answer = parse_and_negate_problem(f)
-    with open(f.replace('.txt', '_negated.txt'), 'w') as f:
-        f.write(file_template.format(context=context, question=question, answer=answer))
+    context, questions, answer = parse_and_negate_problem(f)
+    for i in range(2):
+        with open(f.replace('.txt', f'_negated_{i}.txt'), 'w') as pf:
+            pf.write(file_template.format(context=context, question=questions[i], answer=answer))
+
     # print('Context:', context)
     # print('Question:', question)
     # print('Answer:', answer)
