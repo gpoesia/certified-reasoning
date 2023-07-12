422f67d0a6b0a266300ca7ba58e8d0f9411bdfd6
Gabriel Poesia Reis e Silva <me@gpoesia.com>
2023-07-11 18:09:36 -0700

    Remove files from the Peano repo that are useless here

--- demo/peano-demo/lib/api.js
+++
@@ -1,6 +0,0 @@
-module.exports.apiRequest = async (endpoint, parameters={}) => {
-  const req = await fetch('/api/' + endpoint + '?params=' + encodeURIComponent(JSON.stringify(parameters)),
-                          { method: 'POST' });
-  const res = await req.json();
-  return res;
-}
